dfe362a6bad1e0bdacbbb631b44c68b7f38ee359
n6mya <g4tyaa@gmail.com>
2024-11-15 12:28:32 +0700

    add script to create grid in DOM

---
+++ script.js
@@ -0,0 +1,12 @@
+const grid = document.querySelector('#container')
+
+for (i = 0; i < 16; i++){
+    const row = document.createElement("div");
+    for (a = 0; a < 16; a++){
+        const column = document.createElement("div");
+        column.setAttribute("id", "column");
+        row.appendChild(column);
+    }
+    row.setAttribute("id", "row");
+    grid.appendChild(row);
+}
